d0ccaabaeee10dc6cfa1bce3c2a6bd7e5189455b
SUPRADIP DEY <97578674+DESUP2@users.noreply.github.com>
2023-01-10 12:23:54 +0530

    Add files via upload
    Kindly un compress the .7z files to extract folders and files in it.

---
+++ wp-content/themes/wplms/functions.php
@@ -0,0 +1,50 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Essentials
+include_once 'includes/config.php';
+include_once 'includes/init.php';
+
+// Register & Functions
+include_once 'includes/register.php';
+include_once 'includes/actions.php';
+include_once 'includes/filters.php';
+include_once 'includes/func.php';
+include_once 'includes/ratings.php'; 
+// Customizer
+include_once 'includes/customizer/customizer.php';
+include_once 'includes/customizer/css.php';
+include_once 'includes/vibe-menu.php';
+include_once 'includes/notes-discussions.php';
+include_once 'includes/wplms-woocommerce-checkout.php';
+
+if ( function_exists('bp_get_signup_allowed')) {
+    include_once 'includes/bp-custom.php';
+}
+
+include_once '_inc/ajax.php';
+include_once 'includes/buddydrive.php';
+//Widgets
+include_once('includes/widgets/custom_widgets.php');
+if ( function_exists('bp_get_signup_allowed')) {
+ include_once('includes/widgets/custom_bp_widgets.php');
+}
+if (function_exists('pmpro_hasMembershipLevel')) {
+    include_once('includes/pmpro-connect.php');
+}
+include_once('includes/widgets/advanced_woocommerce_widgets.php');
+include_once('includes/widgets/twitter.php');
+include_once('includes/widgets/flickr.php');
+
+//Misc
+include_once 'includes/extras.php';
+include_once 'includes/tincan.php';
+include_once 'setup/wplms-install.php';
+
+include_once 'setup/installer/envato_setup.php';
+
+// Options Panel
+get_template_part('vibe','options');
+
+
